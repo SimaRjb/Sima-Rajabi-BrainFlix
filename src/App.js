f67d9288165f3fbc8ab73416a6db8ac6d59e1d47
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-20 01:21:45 -0800

    updated the name of hero component to MainVideo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Header/Header'
 import Header from './components/Header/Header';
-import MainVideo from './components/Hero/MainVideo';
+import MainVideo from './components/MainVideo/MainVideo';
 import Comments from './components/Comments/Comments';
 function App() {
   return (
