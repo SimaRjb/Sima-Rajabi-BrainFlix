4263ebcca5e41afb9b5e763a78ca99a22c513ccd
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-20 19:59:31 -0800

    added mainVideo Poster

--- src/components/MainVideo/MainVideo.js
+++ src/components/MainVideo/MainVideo.js
@@ -1,16 +1,19 @@
+
 import './MainVideo.scss';
-import avatar from "../../assets/images/Mohan-muruge.jpg";
+
+const mainVideoPoster = "https://project-2-api.herokuapp.com/images/image0.jpg";
 
 function MainVideo() {
   return (
     <section className='main-video'>
-        <div className='main-video__wrapper'>
-            <video className='main-video__featured' poster={avatar} controls>
-
-            </video>
-        </div>
+      <div className='main-video__wrapper'>
+        <video className='main-video__featured' alt="The Selected Video" poster={mainVideoPoster} controls>
+        {/* <video className='main-video__featured' alt="The Selected Video" poster={mainVideo} controls> */}
+        </video>
+      </div>
     </section>
   );
 }
 
 export default MainVideo;
+
