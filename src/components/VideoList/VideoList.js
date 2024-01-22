72657c3a57ebff67a4c5268ef6e57e5a834c0c84
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-21 19:09:07 -0800

    updated the styling of video list

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -7,24 +7,48 @@ function VideoList({ videoListData, mainVideo, alterVideo }) {
     <section className="videos">
       <div className="videos__wrapper">
         <ul className="videos__list">
-          <li></li>
-          <li></li>
+          {/* <li>
+            <div className="video__wrapper">
+              <div className="video__img-wrapper">
+                <img className="video__img" src="" alt="poster" />
+              </div>
 
+              <div className="video__title-wrapper">
+                <h3 className="video__title"></h3>
+              </div>
+
+              <div className="video__channel-wrapper">
+                <p className="video__channel"></p>
+              </div>
+            </div>
+          </li>
+          <li></li> */}
 
           {videoListData.map((video) => {
             return (
-              video.id !== mainVideo.id &&
-              <li
-                key={video.id}
-                onClick={() => {
-                  console.log(video);
-                  alterVideo(video.id);
-                }}
-              >
-                {video.title}
-                {video.channel}
-                <img className="video__img" src={video.image} alt="poster" />
-              </li>
+              video.id !== mainVideo.id && (
+                <li
+                  key={video.id}
+                  onClick={() => {
+                    console.log(video);
+                    alterVideo(video.id);
+                  }}
+                >
+                  <div className="video__wrapper">
+                    <div className="video__img-wrapper">
+                      <img className="video__img" src={video.image} alt="poster" />
+                    </div>
+
+                    <div className="video__title-wrapper">
+                      <h3 className="video__title">{video.title}</h3>
+                    </div>
+
+                    <div className="video__channel-wrapper">
+                      <p className="video__channel">{video.channel}</p>
+                    </div>
+                  </div>
+                </li>
+              )
             );
           })}
         </ul>
