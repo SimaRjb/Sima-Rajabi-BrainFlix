823e3f499e4eb15a8a1acf46be69e79d1709194b
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-27 14:46:40 -0800

    added home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,14 @@
 import "./HomePage.scss";
 import MainVideo from "../../components/MainVideo";
 import VideoList from "../../components/VideoList";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 
 function HomePage(){
+
     return(
         <main>
+            <MainVideo/>
 
         </main>
     )
