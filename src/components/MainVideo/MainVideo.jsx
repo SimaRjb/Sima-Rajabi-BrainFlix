8221f812f0b7b88d736ef013c4652ff4d152f6a7
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-27 17:04:48 -0800

    added receiving data from api

---
+++ src/components/MainVideo/MainVideo.jsx
@@ -0,0 +1,36 @@
+import "./MainVideo.scss";
+// import "../../data/videos.json";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+
+function MainVideo(){
+
+
+
+  return(
+    <main>
+
+    </main>
+  )
+}
+
+
+// function MainVideo({ mainVideo }) {
+//   return (
+//     <section className="main-video">
+//       <div className="main-video-container">
+//         <div className="main-video__wrapper">
+//           <video
+//             className="main-video__featured"
+//             alt="The Selected Video"
+//             poster={mainVideo.image}
+//             controls
+//           ></video>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+export default MainVideo;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,65 @@
 import "./HomePage.scss";
-import MainVideo from "../../components/MainVideo";
-import VideoList from "../../components/VideoList";
-import { useEffect } from "react";
+import MainVideo from "../../components/MainVideo/MainVideo";
+import VideoList from "../../components/VideoList/VideoList";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import axios from "axios";
 
-function HomePage(){
+function HomePage() {
+  const baseUrl = "https://project-2-api.herokuapp.com";
+  let apiKey = null; //global variable to store the API Key
 
-    return(
-        <main>
-            <MainVideo/>
+  const { videoId } = useParams();
+  //  videoId = "84e96018-4022-434e-80bf-000ce4cd12b8"
+  const [videoDetails, setVideoDetails] = useState({});
 
-        </main>
-    )
+  const fetchApiKey = async () => {
+    try {
+      const res = await axios.get(`${baseUrl}/register`);
+      apiKey = res.data;
+      console.log(apiKey);
+    } catch (error) {
+      console.error("Error fetching API key: ", error);
+    }
+  };
+  (async () => {
+    await fetchApiKey();
+  })();
+
+  const fetchVideoDetails = async () => {
+    try {
+      if (videoId) {
+        const resVideoDetails = await axios.get(
+          `${baseUrl}/videos/${videoId}?api_key=${apiKey}`
+        );
+        setVideoDetails(resVideoDetails.data);
+        console.log(resVideoDetails.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchVideoDetails();
+  }, []);
+
+  //  const fetchVideoList = async () =>{
+  //     try {
+  //         const resVideoList = await axios.get(`${baseUrl}/videos?api_key=${apiKey}`)
+  //         console.log(resVideoList.data);
+  //     } catch (error) {
+  //         console.error(error);
+  //     }
+  //  }
+
+  //  (async() =>{
+  //     fetchVideoList();
+  //  })();
+
+  return (
+    <main>
+      <MainVideo />
+    </main>
+  );
 }
 export default HomePage;
