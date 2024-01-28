f92f0f7371885e1e6894b29a0a50c30bdefe5cbe
Sima rajabi <sima.rajabi24@gmail.com>
2024-01-27 18:20:56 -0800

    edited the structure of api call in home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,41 +7,43 @@ import axios from "axios";
 
 function HomePage() {
   const baseUrl = "https://project-2-api.herokuapp.com";
-  let apiKey = null; //global variable to store the API Key
+//   let apiKey = null; //global variable to store the API Key
 
   const { videoId } = useParams();
   //  videoId = "84e96018-4022-434e-80bf-000ce4cd12b8"
   const [videoDetails, setVideoDetails] = useState({});
 
+  const [apiKey, setApiKey] = useState(null);
   const fetchApiKey = async () => {
     try {
       const res = await axios.get(`${baseUrl}/register`);
-      apiKey = res.data;
+      setApiKey(res.data);
       console.log(apiKey);
     } catch (error) {
       console.error("Error fetching API key: ", error);
     }
   };
-  (async () => {
-    await fetchApiKey();
-  })();
+useEffect(()=>{
+    fetchApiKey();
+},[])
 
-  const fetchVideoDetails = async () => {
-    try {
-      if (videoId) {
-        const resVideoDetails = await axios.get(
-          `${baseUrl}/videos/${videoId}?api_key=${apiKey}`
-        );
-        setVideoDetails(resVideoDetails.data);
-        console.log(resVideoDetails.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    fetchVideoDetails();
-  }, []);
+//   const fetchVideoDetails = async () => {
+//     try {
+//       if (videoId) {
+//         const resVideoDetails = await axios.get(
+//           `${baseUrl}/videos/${videoId}?api_key=${apiKey}`
+//         );
+//         setVideoDetails(resVideoDetails.data);
+//         console.log("resVideoDetails: ", resVideoDetails.data);
+//         console.log("video details : ", videoDetails);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   useEffect(() => {
+//     fetchVideoDetails();
+//   }, []);
 
   //  const fetchVideoList = async () =>{
   //     try {
@@ -58,7 +60,11 @@ function HomePage() {
 
   return (
     <main>
-      <MainVideo />
+     <div>
+    {/* Render component content here */}
+    {/* {console.log(apiKey)} */}
+  </div>
+      <MainVideo videoId={videoId}/>
     </main>
   );
 }
